a844358700e7d3bbc3e3686da634635e5fba9743
zhouguo <378046836@qq.com>
2022-07-14 10:34:58 +0800

    test

---
+++ .vuepress/components/GithubInfos.vue
@@ -0,0 +1,13 @@
+<template>
+    <span class="git-hub">
+		<github-button href="https://github.com/zg/awesome-fenix" data-icon="octicon-star" data-show-count="true" aria-label="Star zg/awesome-fenix on GitHub">Star</github-button>
+		<github-button href="https://github.com/zg" data-show-count="true" aria-label="Follow @zg on GitHub">Follow</github-button>
+		<github-button href="https://github.com/zg/awesome-fenix/fork" data-icon="octicon-repo-forked" data-show-count="true" aria-label="Fork zg/awesome-fenix on GitHub">Fork</github-button>
+	</span>
+</template>
+
+<script>
+    export default {
+        name: "GithubInfos"
+    }
+</script>
